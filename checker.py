48872e2828ef80165e093359c719a113954bc258
vladalexgit <vldflorea@gmail.com>
2018-05-09 17:12:56 +0300

    can reuse old containers

--- checker.py
+++ checker.py
@@ -1,6 +1,6 @@
-import os
-import sys
-import docker
+import time
+
+from containers import manager
 
 from honeypots.honeypot import Honeypot
 from tests.test import TestResult
@@ -9,73 +9,22 @@ from test_platform import TestPlatform
 from tests.http_test import HTTPTest
 from tests.smtp_test import SMTPTest
 
+# test artillery
 
-def start_container(name, client):
+manager.start_honeypot('artillery')
 
-    # TODO make sure the user is enrolled in the docker group
+time.sleep(10)
 
-    # build the image
-    # TODO check if image already exists first and reuse it
+hp = Honeypot(manager.get_honeypot_ip('artillery'), False)
 
-    output = client.build(path=os.path.join('containers', name), tag='artillery')
+tp = TestPlatform([SMTPTest(hp), HTTPTest(hp)])
 
-    for response in output:
-        print(response)
-        # if 'error' in response:
-        #    raise Exception("Error building docker image: {}".format(response['error']))
+tp.run_tests()
 
-    # create container based on image
+results = tp.get_results()
 
-    container = client.create_container(image='artillery', detach=True)
+for tname, treport, tresult in results:
+    print(tname, " ---> ", tresult)
+    print("\t", treport)
 
-    # start the container
-
-    client.start(container)
-
-    # return the container
-
-    return container
-
-
-def get_container_ip(container, client):
-
-    container_details = client.inspect_container(container)
-    target_ip = container_details['NetworkSettings']['IPAddress']
-
-    return target_ip
-
-
-def cleanup_honeypot(name, container, client):
-
-    client.stop(container)
-    client.remove_container(container, force=True)
-    client.remove_image(name, force=True)
-
-
-def main():
-
-    client = docker.from_env()
-
-    # test artillery
-
-    container = start_container('artillery', client)
-
-    hp = Honeypot(get_container_ip(container, client), False)
-
-    tp = TestPlatform([SMTPTest(hp), HTTPTest(hp)])
-
-    tp.run_tests()
-
-    results = tp.get_results()
-
-    for tname, treport, tresult in results:
-        print(tname, " ---> ", tresult)
-        print("\t", treport)
-
-    print(tp.get_stats())
-
-    cleanup_honeypot('artillery', container, client)
-
-
-if __name__ == '__main__':
-    main()
+print(tp.get_stats())
---
+++ containers/manager.py
@@ -0,0 +1,65 @@
+import os
+import docker
+import docker.errors
+
+
+client = docker.from_env()
+
+
+def start_honeypot(name):
+
+    # TODO make sure the user is enrolled in the docker group in the modules init
+
+    # check if the container exists
+
+    try:
+        client.inspect_container(name)
+    except docker.errors.NotFound:
+        print("Container ", name, " not found, creating new container from image ...")
+
+        # create container based on image
+
+        try:
+            client.create_container(image=name, detach=True, name=name)
+        except docker.errors.NotFound:
+            # build the image
+            output = client.build(path=os.path.join(os.path.dirname(__file__), name), tag=name)
+
+            for line in output:
+                print(line)
+
+            client.create_container(image=name, detach=True, name=name)
+
+    else:
+        print("Container ", name, " found")
+
+    # start the container
+
+    client.start(name)
+
+
+def get_honeypot_ip(name):
+
+    container_details = client.inspect_container(name)
+    target_ip = container_details['NetworkSettings']['IPAddress']
+
+    return target_ip
+
+
+def stop_honeypot():
+    pass
+
+
+def stop_all_honeypots():
+    pass
+
+
+def cleanup_honeypot(name):
+
+    client.stop(name)
+    client.remove_container(name, force=True)
+    client.remove_image(name, force=True)
+
+
+def cleanup_all_honeypots():
+    pass
