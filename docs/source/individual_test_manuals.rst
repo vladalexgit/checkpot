61f0c00c15e844c2a43b6d0b7fa6f5437b01eb15
vladalexgit <vldflorea@gmail.com>
2018-08-02 23:19:55 +0300

    added links to suggestions in test results

---
+++ docs/source/individual_test_manuals.rst
@@ -0,0 +1,8 @@
+Test manuals
+============
+
+.. toctree::
+   :maxdepth: 4
+
+   test_manuals/_not_found_manual
+   test_manuals/default_banner
--- tests/default_ftp.py
+++ tests/default_ftp.py
@@ -8,6 +8,7 @@ class DefaultFTPBannerTest(Test):
     name = "Default FTP Banner Test"
     description = "Tests usage of default service banners"
     karma_value = 100
+    doc_file = "default_banner.html"
 
     def run(self):
         """Check if banner matches any known banner"""
